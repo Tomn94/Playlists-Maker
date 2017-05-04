cc87dc9b345ba0fd4ce43229f41dbf9911b12f9e
tomn94 <tmnstudios@hotmail.fr>
2017-05-04 19:49:16 +0800

    + Load songs in library + Song structure implementation + Genre enum

--- Playlist Maker/Model/Library.swift
+++ Playlist Maker/Model/Library.swift
@@ -9,42 +9,87 @@
 import Foundation
 import MediaPlayer
 
+/// Contains all information about the current songs to sort
 class Library {
     
-    var currentSong: Song?
+//    var currentSong: Song?
     
     
     // MARK: Status
     
     typealias LibraryStatus = MPMediaLibraryAuthorizationStatus
     
+    /// Get current access status from iOS
     var status: LibraryStatus {
         return MPMediaLibrary.authorizationStatus()
     }
     
+    
+    /// Ask access to iOS music library
+    ///
+    /// - Parameter handler: Block called after the user choses whether to authorize the app
     func askAuthorization(handler: @escaping (LibraryStatus) -> Void) {
+        
         MPMediaLibrary.requestAuthorization(handler)
     }
     
     
     // MARK: Content
     
-    var songs: [Song] {
-        return []
-    }
+    /// List of the songs to sort
+    var songs = [Song]()
     
-    var artists: [Artist] {
-        return []
+    /// Fill library with songs to sort
+    func load() {
+        
+        /* Get raw songs in library focus */
+        let librarySongs = MPMediaQuery.songs().items ?? []
+        var songs = [Song]()
+        
+        /* Create Song and add to list */
+        for songItem in librarySongs {
+            songs.append(Song(item: songItem))
+        }
+        
+        self.songs = songs
     }
     
 }
 
 
-class Song {
+struct Song {
+    
+    /// Default maximum artwork size displayed
+    static let artworkSize = CGSize(width: 100, height: 100)
+    
+    let title: String
+    
+    let artist: String//Artist
+    
+    let album: String?//Album
+    
+    let genre: String?//Genre
+    
+    let length: TimeInterval
+    
+    let artwork: UIImage?
+    
+    /// Init Song object with a media library query result
+    ///
+    /// - Parameter item: Library item to wrap in Song object
+    init(item: MPMediaItem) {
+        
+        self.title   = item.title ?? "Unknown title"
+        self.artist  = item.artist ?? "Unknown artist"
+        self.album   = item.albumTitle ?? "Unknown album"
+        self.genre   = item.genre
+        self.length  = item.playbackDuration
+        self.artwork = item.artwork?.image(at: Song.artworkSize)
+    }
     
 }
 
-class Artist {
+struct Artist {
     
     var albums: [Album] {
         return []
@@ -52,6 +97,10 @@ class Artist {
     
 }
 
-class Album {
+struct Album {
     
+}
+
+enum Genre: String {
+    case all
 }
