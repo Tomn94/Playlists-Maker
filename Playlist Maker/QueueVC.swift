051acf238c11b176b1cbc82eff6d65862bc477dd
tomn94 <tmnstudios@hotmail.fr>
2017-08-31 03:48:54 +0200

    + Separate songs done and to do + Scroll to current song + Indicate the number of songs (total & each section)

--- Playlist Maker/QueueVC.swift
+++ Playlist Maker/QueueVC.swift
@@ -13,22 +13,47 @@ import UIKit
 class QueueVC: UIViewController {
 
     /// Model
-    let songs = DataStore.shared.library.songs
+    let songs: [[Song]] = {
+        let allSongs     = DataStore.shared.library.songs
+        let currentIndex = DataStore.shared.currentIndex ?? 0
+        
+        /* Cut sections at current index */
+        let done = allSongs[0 ..< currentIndex]
+        let todo = allSongs[currentIndex ..< allSongs.count]
+        
+        return [Array(done), Array(todo)]
+    }()
     
     /* UI */
     /// Table view containing listed songs
     @IBOutlet weak var tableView: UITableView!
+    
     /// Main view containing the table view on top of a blur effect
     @IBOutlet weak var visualEffectView: UIVisualEffectView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let count = DataStore.shared.library.songs.count
+        if count == 1 {
+            title = "Sorting 1 Song"
+        } else {
+            title = "Sorting \(count) Songs"
+        }
 
         /* Apply vibrancy on cell separators using the same blur effect */
         if let effect = visualEffectView.effect as? UIBlurEffect {
             tableView.separatorEffect = UIVibrancyEffect(blurEffect: effect)
         }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        /* Always show the current song */
+        guard songs.count > 0 else { return }
+        tableView.scrollToRow(at: IndexPath(row: 0, section: 1), at: .middle, animated: false)
     }
 
 }
@@ -36,15 +61,45 @@ class QueueVC: UIViewController {
 // MARK: - Table View Data Source
 extension QueueVC: UITableViewDataSource {
     
+    /// Determines the number of sections in the list
+    ///
+    /// - Parameter tableView: Songs list view
+    /// - Returns: 2 sections, Done & To Do
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return songs.count
+    }
+    
     /// Determines the number of rows in the list
     ///
     /// - Parameters:
     ///   - tableView: Songs list view
-    ///   - section: One and only section
+    ///   - section: Section index
     /// - Returns: Number of songs
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        return songs.count
+        
+        return songs[section].count
+    }
+    
+    /// Fills the section names
+    ///
+    /// - Parameters:
+    ///   - tableView: Songs list view
+    ///   - section: Section index
+    /// - Returns: Number of songs in section and section title
+    func tableView(_ tableView: UITableView,
+                   titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0 {
+            let count = songs[0].count
+            if count == 1 {
+                return "1 Sorted Song"
+            }
+            return "\(count) Sorted Songs"
+        }
+        
+        return "\(songs[1].count) Remaining"
     }
     
     /// Populates the songs list with rows
@@ -58,7 +113,7 @@ extension QueueVC: UITableViewDataSource {
         
         /* Get cell & its model data */
         let cell = tableView.dequeueReusableCell(withIdentifier: "queueCell", for: indexPath)
-        let song = songs[indexPath.row]
+        let song = songs[indexPath.section][indexPath.row]
         
         /* Put Artist & Album names together, separated by an em dash */
         var detailText = ""
