2536a031ba1f9950f83bb5543ddb0c2744a32384
Thomas Naudet <tmnstudios@hotmail.fr>
2017-09-02 19:28:45 +0200

    ~ Fixed tracks for .notInSelectedPlaylists
    + Introduced a notInPlaylists(_:) common for most cases

--- Playlist Maker/Model/LibraryQueries.swift
+++ Playlist Maker/Model/LibraryQueries.swift
@@ -8,49 +8,56 @@
 
 import MediaPlayer
 
+/// List of already-available media requests filtering songs in Library.
 class LibraryQueries {
     
+    /// Returns all songs in library
     class var allSongs: Set<MPMediaItem> {
         return Set(MPMediaQuery.songs().items ?? [])
     }
     
+    /// Returns all songs contained in `DataStore.shared.library.selectionInPlaylists`
     class var inSelectedPlaylists: Set<MPMediaItem> {
         
         let playlists = DataStore.shared.library.selectionInPlaylists
         return LibraryQueries.inPlaylists(playlists)
     }
     
+    /// Returns all songs not contained in `DataStore.shared.library.selectionInPlaylists`.
     class var notInSelectedPlaylists: Set<MPMediaItem> {
         
-        let allSongs    = LibraryQueries.allSongs
-        let inPlaylists = LibraryQueries.inSelectedPlaylists
-        
-        return allSongs.subtracting(inPlaylists)
+        let selectionNotInPlaylists = DataStore.shared.library.selectionNotInPlaylists
+        return LibraryQueries.notInPlaylists(selectionNotInPlaylists)
     }
     
     class var notInDestinationPlaylists: Set<MPMediaItem> {
         
-        let allSongs   = LibraryQueries.allSongs
-        let playlists  = DataStore.shared.library.destinationPlaylists
-        let allInDests = LibraryQueries.inPlaylists(playlists)
-        
-        return allSongs.subtracting(allInDests)
+        let destinationPlaylists = DataStore.shared.library.destinationPlaylists
+        return LibraryQueries.notInPlaylists(destinationPlaylists)
     }
     
     class var inNoPlaylists: Set<MPMediaItem> {
     
-        let allSongs       = LibraryQueries.allSongs
-        let playlists      = DataStore.shared.library.playlists
-        let allInPlaylists = LibraryQueries.inPlaylists(playlists)
+        let allPlaylists = DataStore.shared.library.playlists
+        return LibraryQueries.notInPlaylists(allPlaylists)
         
         return allSongs.subtracting(allInPlaylists)
     }
     
     
+    // MARK: - Common
+    
+    /// Returns all songs in library that are contained in the given playlists
+    ///
+    /// - Parameter playlists: Playlists whose songs will be included
+    /// - Returns: Unique set of songs listed in those playlists
     private class func inPlaylists(_ playlists: [Playlist]) -> Set<MPMediaItem> {
         
         var librarySongs = Set<MPMediaItem>()
         
+        /* Go through each playlist
+           NB: There could be a filter
+           */
         for playlist in playlists {
             
             let query = MPMediaQuery.songs()
@@ -65,6 +72,19 @@ class LibraryQueries {
         }
         
         return librarySongs
+    }
+    
+    /// Returns all songs in library that are not contained in any given playlist.
+    /// Inverts `inPlaylists(_:)`
+    ///
+    /// - Parameter playlists: Playlists whose songs will be excluded
+    /// - Returns: Difference between Library songs and songs in playlists
+    private class func notInPlaylists(_ playlists: [Playlist]) -> Set<MPMediaItem> {
+        
+        let allSongs       = LibraryQueries.allSongs
+        let allInPlaylists = LibraryQueries.inPlaylists(playlists)
+        
+        return allSongs.subtracting(allInPlaylists)
     }
     
 }
